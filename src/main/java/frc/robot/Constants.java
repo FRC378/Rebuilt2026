66cfce9c8e8635068a66cd179eba0cb617d08187
Team378Programmer <Team378Programmer@gmail.com>
2026-02-07 14:49:32 -0500

    Started Intake code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,8 +10,8 @@ public final class Constants {
   public static final int    PDP_CAN_ID       = 2;
   public static final int    PIGEON_CAN_ID    = 10;
   public static final int    CLIMBER_MOTOR_CAN_ID = 11; 
-  
-
+  public static final int    COLLECTOR_MOTOR_CAN_ID = 12;
+  public static final int    ARM_MOTOR_CAN_ID = 9; 
 
 
 
--- src/main/java/frc/robot/subsystems/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake.java
@@ -3,15 +3,71 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.subsystems;
+import java.util.stream.Collector;
+
+import com.revrobotics.PersistMode;
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.ResetMode;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Intake extends SubsystemBase {
-  /** Creates a new Intake. */
-  public Intake() {}
+  private SparkMax m_collectorMotor;
+  private SparkMax m_armMotor; 
+  private RelativeEncoder m_armEncoder; 
+
+
+  
+  public Intake() {
+    m_collectorMotor = new SparkMax(Constants.COLLECTOR_MOTOR_CAN_ID , MotorType.kBrushless);
+    m_armMotor = new SparkMax(Constants.ARM_MOTOR_CAN_ID , MotorType.kBrushless); 
+    m_armEncoder = m_armMotor. getEncoder();
+
+    SparkMaxConfig collectorMotorConfig = new SparkMaxConfig(); 
+
+    collectorMotorConfig
+      .smartCurrentLimit(40)
+      .idleMode(IdleMode.kCoast)
+      .inverted(false) 
+      .openLoopRampRate(0.0);
+
+    m_collectorMotor.configure(collectorMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    SparkMaxConfig armMotorConfig = new SparkMaxConfig(); 
+
+    armMotorConfig
+      .smartCurrentLimit(40)
+      .idleMode(IdleMode.kBrake)
+      .inverted(false) 
+      .openLoopRampRate(0.0);
+
+    m_armMotor.configure(armMotorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+  }
+
+
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+  
+
+
   }
+  public void ArmGo(double power) {
+    m_armMotor.set(power);}
+
+  public void CollectorGo(double power) {
+    m_collectorMotor.set(power);}
+
+  public void IntakeAllStop() {
+    m_armMotor.set(0.0); 
+    m_collectorMotor.set(0.0);
+  }
+
+  
 }
